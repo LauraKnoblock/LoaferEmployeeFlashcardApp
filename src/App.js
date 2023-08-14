75c19503c3e9a3227adf7ec0c30e71fc6e6e2e04
Your Name <you@example.com>
2023-08-14 13:32:12 -0500

    added more specific categories

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
 
   const categoryFlashcards = {
     bread: SAMPLE_FLASHCARDS.filter(card => card.category === 'bread'),
-    lunch: SAMPLE_FLASHCARDS.filter(card => card.category === 'lunch'),
+    sandwiches: SAMPLE_FLASHCARDS.filter(card => card.category === 'sandwiches'),
+    lunchSoupSalad: SAMPLE_FLASHCARDS.filter(card => card.category === 'lunchSoupSalad'),
+    snacksSides: SAMPLE_FLASHCARDS.filter(card => card.category === 'snacksSides'),
+    sauces: SAMPLE_FLASHCARDS.filter(card => card.category === 'sauces'),
     pizza: SAMPLE_FLASHCARDS.filter(card => card.category === 'pizza'),
   };
 
@@ -19,31 +22,6 @@ function App() {
     setFlashcards(categoryFlashcards[newCategory]);
   };
 
-
-  // useEffect(() => {
-  //   axios
-  //   .get('https://opentdb.com/api.php?amount=10')
-  //   .then(res => {
-  //     setFlashcards(res.data.results.map((questionItem, index) => {
-  //       const answer = decodeString(questionItem.correct_answer);
-  //       const options = [
-  //         ...questionItem.incorrect_answers.map(a => decodeString(a)), answer]
-  //       return {
-  //         id: `${index}-${Date.now()}`,
-  //         question: decodeString(questionItem.question),
-  //         answer: questionItem.correct_answer,
-  //         options: options.sort(() => Math.random() - .5)
-  //       }
-  //     }))
-  //     console.log(res.data)
-  //   })
-  // }, [])
-
-  // function decodeString(str) {
-  //   const textArea = document.createElement('textarea')
-  //   textArea.innerHTML= str;
-  //   return textArea.value;
-  // }
   return (
     <div>
       <div className="header">
@@ -54,8 +32,12 @@ function App() {
         <select value={selectedCategory} onChange={handleCategoryChange}>
         <option value="none" disabled>Select a category to study</option>
           <option value="bread">Bread</option>
-          <option value="lunch">Lunch</option>
+          <option value="sandwiches">Sandwiches</option>
+          <option value="lunchSoupSalad">Lunch: Soup & Salad</option>
+          <option value='snacksSides'>Snacks & Sides</option>
+          <option value='sauces'>Sauces</option>
           <option value="pizza">Pizza</option>
+          <option value = "dinnerStarters">Dinner Starters</option>
         </select>
       </div>
       <div className="container">
@@ -71,37 +53,31 @@ const SAMPLE_FLASHCARDS = [
       name: `Light & Mild`,
       category: 'bread',
       image: 'https://129144036.cdn6.editmysite.com/uploads/1/2/9/1/129144036/s642576876884555124_p4_i3_w1280.jpeg?dpr=1.25',
-      description:`This is a very traditional sourdough in the style of rustic French countryside bread`,
+      description:`Our flagship sourdough or "pain au levain," this loaf has an open, custardy crumb and a brittle and crackery crust. Light and mild features a mild sourness and creamy, sweet wheat flavors.`,
       ingredient: `Ingredients
       Transitional Whole Kernel wheat flour from The Mill at Janie’s Farm (for nutrition and flavor),
       Dark rye flour from Janies,
       High protein bread flour from Janie’s (for gluten strength),
       Water,
-      Salt`,
-      options: [
-          // '2', '3', '4', '5',
-      ]
+      Salt`
   },
   {
       id: 2,
       name: `Janie's Bread`,
       category: 'bread',
-      image: 'https://129144036.cdn6.editmysite.com/uploads/1/2/9/1/129144036/s642576876884555124_p232_i5_w2880.jpeg?width=1280&dpr=1.25',
+      image: 'https://129144036.cdn6.editmysite.com/uploads/1/2/9/1/129144036/s642576876884555124_p112_i3_w2048.jpeg?width=1280&dpr=1.25',
       description: `This bread is made with a local Whole Wheat that is stone-milled fresh for us in Ashkum, IL`,
       ingredient: `Transitional Whole Kernel wheat flour from The Mill at Janie's Farm (for nutrition and flavor)
       Dark rye flour from Janies 
       High protein bread flour from Janie's (for gluten strength)
       Water
-      Salt
-      `,
-      options: [
-          // 'Answer', 'Answer 1', 'Answer 2', 'Answer 3',
-      ]
+      Salt`
   },
   {
     id: 3,
     name: `Hoagie`,
     category: 'bread',
+    image: 'https://129144036.cdn6.editmysite.com/uploads/1/2/9/1/129144036/s642576876884555124_p355_i1_w2048.jpeg?width=1280&dpr=1.25',
     description: `Hoagies are a classic sandwich shop bread. It’s a soft white bread with a tender crumb and mild flavor.`,
     ingredient: `high gluten wheat
     Durum wheat
@@ -109,10 +85,7 @@ const SAMPLE_FLASHCARDS = [
     Sugar
     Instant yeast
     Salt
-    Soy oil`,
-    options: [
-        // 'Answer', 'Answer 1', 'Answer 2', 'Answer 3',
-    ]
+    Soy oil`
 },
 {
   id: 4,
@@ -125,15 +98,13 @@ const SAMPLE_FLASHCARDS = [
   Soy Oil
   Water
   Salt
-  Sesame Seeds`,
-  options: [
-      // 'Answer', 'Answer 1', 'Answer 2', 'Answer 3',
-  ]
+  Sesame Seeds`
 },
 {
   id: 5,
   name: `Marble Rye`,
   category: 'bread',
+  image: 'https://129144036.cdn6.editmysite.com/uploads/1/2/9/1/129144036/s642576876884555124_p46_i5_w3600.jpeg?width=1280&dpr=1.25',
   description: `This is a classic New York Style Deli rye bread.`,
   ingredient: `High Gluten Wheat
   Whole Rye Flour from Janie’s Mill
@@ -141,30 +112,26 @@ const SAMPLE_FLASHCARDS = [
   Molasses
   Cocoa Noir
   Carraway
-  Salt`,
-  options: [
-      // 'Answer', 'Answer 1', 'Answer 2', 'Answer 3',
-  ]
+  Salt`
 },
 {
   id: 6,
-  name: `Baguette`,
+  name: `Demi-Baguette`,
   category: 'bread',
+  image: 'https://129144036.cdn6.editmysite.com/uploads/1/2/9/1/129144036/s642576876884555124_p758_i2_w4032.jpeg?width=1280&dpr=1.25',
   description: `This is the classic French bread. This bread offers a crispy crust with a soft pillowy interior.`,
   ingredient: `Sifted Wheat
   Whole Wheat
   Rye
   Water
   Salt
-  Yeast`,
-  options: [
-      // 'Answer', 'Answer 1', 'Answer 2', 'Answer 3',
-  ]
+  Yeast`
 },
 {
   id: 7,
   name: `Seeded Janie’s Bread`,
   category: 'bread',
+  image: 'https://129144036.cdn6.editmysite.com/uploads/1/2/9/1/129144036/s642576876884555124_p166_i1_w500.jpeg?dpr=1.25',
   description: `This bread is our Local Whole Wheat with the addition of seeds. It’s rich, earthy, and offers a variety of textures.`,
   ingredient: `Transitional Whole Wheat from The Mill at Janie’s Farm
   Water
@@ -173,15 +140,13 @@ const SAMPLE_FLASHCARDS = [
   Millet
   Pumpkin Seed
   Sunflower Seed
-  Sesame Seed`,
-  options: [
-      // 'Answer', 'Answer 1', 'Answer 2', 'Answer 3',
-  ]
+  Sesame Seed`
 },
 {
   id: 8,
   name: `Cheesy Bread`,
   category: 'bread',
+  image: 'https://129144036.cdn6.editmysite.com/uploads/1/2/9/1/129144036/s642576876884555124_p296_i4_w2048.jpeg?width=1280&dpr=1.25',
   description: `This focaccia dough is baked with a sharp white cheddar cheese and topped with breadcrumbs.`,
   ingredient: `High Gluten Wheat
   Water
@@ -190,15 +155,13 @@ const SAMPLE_FLASHCARDS = [
   Yeast
   White Cheddar
   Breadcrumbs
-  Olive oil `,
-  options: [
-      // 'Answer', 'Answer 1', 'Answer 2', 'Answer 3',
-  ]
+  Olive oil `
 },
 {
   id: 9,
   name: `Pizza Rossa`,
   category: 'bread',
+  image: 'https://129144036.cdn6.editmysite.com/uploads/1/2/9/1/129144036/s642576876884555124_p305_i5_w2048.jpeg?width=1280&dpr=1.250',
   description: `This focaccia dough has a spicy salty tomato topping.`,
   ingredient: `High Gluten Wheat
   Water
@@ -207,22 +170,14 @@ const SAMPLE_FLASHCARDS = [
   Yeast
   Crushed tomatoes
   Maldon sea salt
-  Chili oil
-  `,
-  options: [
-      // 'Answer', 'Answer 1', 'Answer 2', 'Answer 3',
-  ]
+  Chili oil`
 },
 {
   id: 10,
   name: `Bistro Beef`,
-  category: 'lunch',
+  category: 'sandwiches',
   description: ``,
   ingredient: `Hoagie, Bistro sauce, Gruyere cheese, Pickled Peppers, Roast Beef, Maldon sea salt`
-  ,
-  options: [
-      // 'Answer', 'Answer 1', 'Answer 2', 'Answer 3',
-  ]
 },
 
 ]
